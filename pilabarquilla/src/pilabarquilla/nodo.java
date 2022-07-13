4974a2063007c51f964014c0a54469cddbf49cc0
JRVS29 <106562402+JRVS29@users.noreply.github.com>
2022-07-12 23:41:42 -0400

    Add files via upload
    jose valerio, cargo la clase barquilla como la creacion de la pila de las porciones.

---
+++ pilabarquilla/src/pilabarquilla/nodo.java
@@ -0,0 +1,14 @@
+
+package pilabarquilla;
+
+public class nodo {
+    int racion;
+    int toping;
+    nodo sig;
+    public nodo (int agregado, int extra ){
+        racion = agregado;
+        toping = extra;
+        sig = null;
+        
+    }
+}
